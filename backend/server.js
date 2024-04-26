a02ea8683cba7eaa9ca6c7e126108ceb89cd169e
MATHUR-LUV <luvm1403@gmail.com>
2024-04-27 01:29:43 +0530

    new changes

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
@@ -6,6 +7,8 @@ import connectToMongoDB from "./db/db.js";
 import userRoutes from "./routes/user.routes.js";
 // import cors from "cors";
 dotenv.config();
+
+const __dirname = path.resolve();
 // const app = express();
 const PORT = process.env.PORT || 5000;
 import cookieParser from "cookie-parser";
@@ -23,6 +26,10 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
     connectToMongoDB();
